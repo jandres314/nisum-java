f2f1693ed30a22c1f7dff44eff53dd1f1435d3a5
Llimi <jaanosor@bancolombia.com.co>
2024-08-27 19:27:53 -0500

    implementacion generacion token

--- applications/app-service/src/main/java/co/com/nisum/config/UseCasesConfig.java
+++ applications/app-service/src/main/java/co/com/nisum/config/UseCasesConfig.java
@@ -1,19 +1,16 @@
 package co.com.nisum.config;
 
 import co.com.nisum.model.user.gateways.UserRepository;
+import co.com.nisum.model.user.gateways.UserToken;
 import co.com.nisum.usecase.user.UserUseCase;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import reactor.core.publisher.Mono;
-
-import java.util.UUID;
 
 @Configuration
 public class UseCasesConfig {
 
     @Bean
-    public UserUseCase userUseCase(UserRepository userRepository) {
-        return new UserUseCase(userRepository, () -> Mono.just(UUID.randomUUID().toString()));
+    public UserUseCase userUseCase(UserRepository userRepository, UserToken userToken) {
+        return new UserUseCase(userRepository, userToken);
     }
 }
---
+++ infrastructure/driven-adapters/jwt-generation/src/main/java/co/com/nisum/jwtgeneration/TokenService.java
@@ -0,0 +1,41 @@
+package co.com.nisum.jwtgeneration;
+
+import co.com.nisum.model.user.gateways.UserToken;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+public class TokenService implements UserToken {
+
+    @Value("${app.token.secretKey}")
+    private String secretKeyToken;
+
+    @Value("${app.token.keyId}")
+    private String keyIdToken;
+
+    @Value("${app.token.subject}")
+    private String subjectToken;
+
+    @Value("${app.token.issuer}")
+    private String issuerToken;
+
+    @Override
+    public Mono<String> createToken() {
+        var algorithm = Algorithm.HMAC512(secretKeyToken);
+        var token = JWT.create()
+                .withIssuer(issuerToken)
+                .withExpiresAt(Instant.now().plusSeconds(900))
+                .withKeyId(keyIdToken)
+                .withJWTId(UUID.randomUUID().toString())
+                .withSubject(subjectToken)
+                .sign(algorithm);
+        return Mono.just(token);
+    }
+
+}
